d084e3a06cd2bb7e4ebbf7529fba67bed6a38d34
Alisson Francisco Krul <alisson.krul@hotmail.com>
2020-02-01 23:29:37 -0300

    follow camera

---
+++ Assets/FollowPlayers.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+
+
+public class FollowPlayers : MonoBehaviour
+{
+
+
+    public List<Transform> targets;
+    public Vector3 velocity;
+    public Vector3 offSet;
+    public float smoothTime = 0.5f;
+    public float minZoom = 2f;
+    public float maxZoom = 20f;
+    public float zoomLimiter = 20f;
+    private Camera cam;
+    [SerializeField]
+    private float camSize;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint(); ;
+
+        Vector3 newPos = centerPoint + offSet;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+
+
+    }
+
+    private void Zoom()
+    {
+        Debug.Log("greaat distance:"+GetGreaatestDistance());
+        float newZoom = Mathf.Lerp(minZoom, maxZoom, GetGreaatestDistance() / zoomLimiter);
+        Debug.Log("new zoom"+newZoom);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+
+        }
+
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+
+        return bounds.center;
+    }
+
+    private float GetGreaatestDistance()
+    {
+
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+        Debug.Log("diagonal:" + Mathf.Sqrt((bounds.size.x * bounds.size.x) + (bounds.size.z * bounds.size.z)));
+        Debug.Log("x:" + bounds.size.x);
+        Debug.Log("y:" + bounds.size.z);
+        return Mathf.Sqrt((bounds.size.x * bounds.size.x) + (bounds.size.z * bounds.size.z));
+    }
+}
---
+++ Assets/[Core]/Scripts/Manager/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    public int timeToPair = 15;
+    public float timeToPairRemaining = 15.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        UpdateTimeToPair();
+    }
+
+
+
+
+    void UpdateTimeToPair() {
+        if (timeToPairRemaining > 0.0)
+        {
+            timeToPairRemaining -= Time.deltaTime;
+        }else{
+            this.TimeToPairEvent();
+        }
+    }
+    void TimeToPairEvent() {
+        Debug.Log("PAAAAIR TIME!");
+
+        //Do wharever you need to do
+        //Don't forget to reset the time to pair:
+        //ResetTimeToPair();
+
+        ResetTimeToPair();
+    }
+    void ResetTimeToPair()
+    {
+        timeToPairRemaining = timeToPair;
+    }
+
+
+
+
+
+}
